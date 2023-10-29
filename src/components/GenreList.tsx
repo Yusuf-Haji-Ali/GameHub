6f5c81ee057a9c9b12262ce047496abacdc97210
Yusuf <yhajiali7@gmail.com>
2023-10-29 12:35:05 +0000

    Show skeletons while fetching genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,24 +1,42 @@
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import {
+  HStack,
+  Image,
+  List,
+  ListItem,
+  Skeleton,
+  SkeletonText,
+  Spinner,
+  Text,
+} from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 
 export const GenreList = () => {
-  const { data } = useGenres();
+  const { data, loading, error } = useGenres();
+
+  if (error) return null;
 
   return (
     <List>
-      {data.map((genre) => (
-        <ListItem key={genre.id} paddingY={"5px"}>
-          <HStack>
-            <Image
-              boxSize={"32px"}
-              borderRadius={8}
-              src={getCroppedImageUrl(genre.image_background)}
-            />
-            <Text fontSize="lg">{genre.name}</Text>
-          </HStack>
-        </ListItem>
-      ))}
+      {loading
+        ? data.map((genre) => (
+            <HStack marginBottom={"5px"}>
+              <Skeleton boxSize={"32px"} key={genre.id} />
+              <SkeletonText noOfLines={1} width={"125px"} />
+            </HStack>
+          ))
+        : data.map((genre) => (
+            <ListItem key={genre.id} paddingY={"5px"}>
+              <HStack>
+                <Image
+                  boxSize={"32px"}
+                  borderRadius={8}
+                  src={getCroppedImageUrl(genre.image_background)}
+                />
+                <Text fontSize="lg">{genre.name}</Text>
+              </HStack>
+            </ListItem>
+          ))}
     </List>
   );
 };
