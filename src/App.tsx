fd8039d9b20fe10b0837d27f181d09d0b771f2e6
Yusuf <yhajiali7@gmail.com>
2023-10-30 13:43:48 +0000

    Clean up: Remove all redundant imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 
 import { Box, Flex, Grid, GridItem, HStack, Show } from "@chakra-ui/react";
 import { NavBar } from "./components/NavBar";
